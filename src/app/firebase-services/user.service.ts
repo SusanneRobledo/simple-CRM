51de0b9afd95084d0ed9abba63d03c6f90efcc7c
SusanneRobledo <susanne.robledo@web.de>
2024-07-04 12:21:45 +0200

    basic generic functions to access data in Firestore

--- src/app/firebase-services/user.service.ts
+++ src/app/firebase-services/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, inject } from '@angular/core';
 import { User } from '../interfaces/user';
-import { Firestore } from '@angular/fire/firestore';
+import { Firestore, collection, doc } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -9,5 +10,13 @@ export class UserService {
   users: User[] = [];
 
   firestore: Firestore = inject(Firestore);
+
   constructor() {}
+  getUsers() {
+    return collection(this.firestore, 'users');
+  }
+
+  getUserId(docId: string) {
+    return doc(collection(this.firestore, 'users'), docId);
+  }
 }
