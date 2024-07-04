e2b3776f9acb69e0bf4192b5e20fdc15ee5e4678
SusanneRobledo <susanne.robledo@web.de>
2024-07-04 09:33:57 +0200

    logo + subscribing to db changes

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,10 +1,12 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { MatTooltip } from '@angular/material/tooltip';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 import { MatCard, MatCardContent } from '@angular/material/card';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-user',
@@ -13,8 +15,19 @@ import { MatCard, MatCardContent } from '@angular/material/card';
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss',
 })
-export class UserComponent {
+export class UserComponent implements OnInit {
   readonly dialog = inject(MatDialog);
+  users: Observable<any[]>;
+
+  constructor(private firestore: Firestore) {}
+
+  ngOnInit(): void {
+    const usersCollection = collection(this.firestore, 'users');
+    this.users = collectionData(usersCollection);
+    this.users.subscribe((changes: any) => {
+      console.log('received changes from DB', changes);
+    });
+  }
 
   openDialog() {
     this.dialog.open(DialogAddUserComponent);
