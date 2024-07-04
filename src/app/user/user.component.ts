54d9a35455bc49b6378e1b06b5cad689736c6211
SusanneRobledo <susanne.robledo@web.de>
2024-07-04 10:05:41 +0200

    display DB changes in app

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -7,25 +7,42 @@ import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.compo
 import { MatCard, MatCardContent } from '@angular/material/card';
 import { Firestore, collection, collectionData } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
+
+interface Users {
+  firstName: string;
+  lastName: string;
+  email: string;
+  city: string;
+}
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [MatButtonModule, MatIcon, MatTooltip, MatCardContent, MatCard],
+  imports: [
+    MatButtonModule,
+    MatIcon,
+    MatTooltip,
+    MatCardContent,
+    MatCard,
+    CommonModule,
+  ],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss',
 })
 export class UserComponent implements OnInit {
   readonly dialog = inject(MatDialog);
-  users: Observable<any[]>;
+  allUsers: Users[] = [];
+  users$: Observable<any> = new Observable();
 
   constructor(private firestore: Firestore) {}
 
   ngOnInit(): void {
     const usersCollection = collection(this.firestore, 'users');
-    this.users = collectionData(usersCollection);
-    this.users.subscribe((changes: any) => {
+    this.users$ = collectionData(usersCollection);
+    this.users$.subscribe((changes: any) => {
       console.log('received changes from DB', changes);
+      this.allUsers = changes;
     });
   }
 
